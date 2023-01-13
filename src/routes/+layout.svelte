1482d78d759a3a0f5cff3bfb1377857181495005
Christian de Botton <416880+cdebotton@users.noreply.github.com>
2023-01-13 00:51:59 -0800

    fix: adjust padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,9 @@
 
 	import '$lib/css/base.css';
 	import { browser } from '$app/environment';
+	import Glass from '$lib/icons/glass.svelte';
+	import Linkedin from '$lib/icons/linkedin.svelte';
+	import Mastodon from '$lib/icons/mastodon.svelte';
 	import { theme } from '$lib/theme';
 
 	const DARK_THEME = 'dark-theme';
@@ -34,9 +37,26 @@
 	<main>
 		<slot />
 	</main>
-	<span class="theme-toggle">
+	<footer class="connect">
+		<ul>
+			<li>
+				<a target="_blank" rel="noreferrer" href="https://linkedin.com/in/christiandebotton">
+					<Linkedin />
+				</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noreferrer" href="https://glass.photo/cdb">
+					<Glass />
+				</a>
+			</li>
+			<li>
+				<a target="_blank" rel="noreferrer" href="https://mas.to/@cdebotton">
+					<Mastodon />
+				</a>
+			</li>
+		</ul>
 		<ThemeToggle />
-	</span>
+	</footer>
 </div>
 
 <style>
@@ -44,17 +64,24 @@
 		display: grid;
 		height: 100%;
 		gap: var(--space-12);
-		grid-template-rows: min-content auto;
+		grid-template-rows: min-content auto min-content;
 		justify-items: center;
 		padding-block: var(--space-5);
 		padding-inline: var(--space-5);
 	}
 
 	header,
-	main {
+	main,
+	footer {
 		width: 100%;
 		min-width: 20rem;
-		max-width: 64rem;
+		max-width: 80rem;
+	}
+
+	header,
+	footer {
+		position: relative;
+		z-index: 10;
 	}
 
 	header {
@@ -69,9 +96,30 @@
 		grid-column: 2 / span 1;
 	}
 
-	.theme-toggle {
-		position: absolute;
-		inset: auto var(--space-4) var(--space-8) auto;
+	.connect {
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+	}
+
+	.connect ul {
+		display: flex;
+		font-size: var(--text-lg);
+		gap: var(--space-4);
+	}
+
+	.connect a {
+		display: inline-block;
+		border-radius: 7px;
+		background-color: var(--mauve4);
+		padding-block: var(--space-2);
+		padding-inline: var(--space-4);
+	}
+
+	@media not all and (hover: none) {
+		.connect a:hover {
+			background-color: var(--mauve5);
+		}
 	}
 
 	@media screen and (min-width: 480px) {
@@ -79,9 +127,12 @@
 			padding-block: var(--space-8);
 			padding-inline: var(--space-8);
 		}
+	}
 
-		.theme-toggle {
-			inset: auto var(--space-10) var(--space-10) auto;
+	@media screen and (min-width: 800px) {
+		div {
+			padding-block: var(--space-8);
+			padding-inline: var(--space-16);
 		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <script>
-	import Glass from '$lib/icons/glass.svelte';
-	import Linkedin from '$lib/icons/linkedin.svelte';
-	import Mastodon from '$lib/icons/mastodon.svelte';
-
 	let sceneLoader = import('$lib/components/scene.svelte');
 </script>
 
@@ -30,30 +26,12 @@
 			<em>product</em>, and <em>user experience</em> engineering teams.
 		</p>
 	</div>
-	<div class="connect">
-		<ul>
-			<li>
-				<a target="_blank" rel="noreferrer" href="https://linkedin.com/in/christiandebotton">
-					<Linkedin />
-				</a>
-			</li>
-			<li>
-				<a target="_blank" rel="noreferrer" href="https://glass.photo/cdb">
-					<Glass />
-				</a>
-			</li>
-			<li>
-				<a target="_blank" rel="noreferrer" href="https://mas.to/@cdebotton">
-					<Mastodon />
-				</a>
-			</li>
-		</ul>
-	</div>
 </div>
 
 <style>
 	.canvas {
-		position: absolute;
+		position: fixed;
+		z-index: 0;
 		inset: 0;
 	}
 
@@ -65,8 +43,7 @@
 		gap: var(--space-12);
 	}
 
-	.intro,
-	.connect {
+	.intro {
 		width: 100%;
 		max-width: 32ch;
 	}
@@ -90,26 +67,6 @@
 		line-height: var(--leading-body);
 	}
 
-	.connect ul {
-		display: flex;
-		font-size: var(--text-lg);
-		gap: var(--space-4);
-	}
-
-	.connect a {
-		display: inline-block;
-		border-radius: 7px;
-		background-color: var(--mauve4);
-		padding-block: var(--space-2);
-		padding-inline: var(--space-4);
-	}
-
-	@media not all and (hover: none) {
-		.connect a:hover {
-			background-color: var(--mauve5);
-		}
-	}
-
 	@media screen and (min-width: 480px) {
 		.content {
 			gap: var(--space-16);
@@ -126,8 +83,7 @@
 			font-weight: 200;
 		}
 
-		.intro,
-		.connect {
+		.intro {
 			max-width: 48ch;
 		}
 	}
